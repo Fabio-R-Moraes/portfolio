0746fb5558b46fce598ac5663d3e81dc8c466415
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2025-04-23 21:33:14 -0300

    Site montado no html e no css

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('portfolio/<int:id>/', views.product),
+    path('', views.index, name='index'),
+    path('portfolio/<int:id>/', views.product, name='product'),
+    path('portfolio/about.html', views.about, name='about'),
+    path('portfolio/curriculum.html', views.curriculum, name='curriculum'),
+    path('portfolio/contact.html', views.contact, name='contact'),
 ]
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -7,3 +7,12 @@ def product(request, id):
     return render(request, 'portfolio/pages/product-view.html', context={
         'nome':'Fábio Rodrigues de Moraes',
     })
+
+def about(request):
+    return render(request, 'portfolio/pages/about.html')
+
+def curriculum(request):
+    return render(request, 'portfolio/pages/curriculum.html')
+
+def contact(request):
+    return render(request, 'portfolio/pages/contact.html')
